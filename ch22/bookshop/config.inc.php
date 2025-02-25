060b0c8ccb7a85673256d6928ea719430bbb5bc8
sorawichpanya <65010914634@msu.ac.th>
2025-02-25 12:02:55 +0700

    a

--- ch22/bookshop/config.inc.php
+++ ch22/bookshop/config.inc.php
@@ -1,8 +1,8 @@
 <?php
 	$host = "localhost" ;				// ชื่อโฮสต์
 	$user = "root" ;					// ชื่อผู้ใช้
-	$pwd="" ;					// รหัสผ่าน
-	$dbName = "ch22" ;			// ชื่อฐานข้อมูล
+	$pwd="12345678P" ;					// รหัสผ่าน
+	$dbName = "shop4634" ;			// ชื่อฐานข้อมูล
 
 	$conn = mysqli_connect($host , $user, $pwd) or die ("ติดต่อฐานข้อมูลล้มเหลว") ;
 	mysqli_query($conn, "SET NAMES utf8") ;
